5875041ff45960cf61696ac24a45c29dc973a1bf
Kawaii-Tsu <LouisP3@learn.cssd.ab.ca>
2017-10-13 12:04:50 -0600

    made ui

---
+++ BMi/ViewController.designer.cs
@@ -0,0 +1,46 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace BMi
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton calculateBtt { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField heightTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField weightTextField { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (calculateBtt != null) {
+                calculateBtt.Dispose ();
+                calculateBtt = null;
+            }
+
+            if (heightTextField != null) {
+                heightTextField.Dispose ();
+                heightTextField = null;
+            }
+
+            if (weightTextField != null) {
+                weightTextField.Dispose ();
+                weightTextField = null;
+            }
+        }
+    }
+}
